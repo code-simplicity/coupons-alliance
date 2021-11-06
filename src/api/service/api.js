4df822bfc51bcd98bc0d00121eb4fa122ba05820
Romantic <72870719+dpy0912@users.noreply.github.com>
2021-11-06 19:34:30 +0800

    编写接口

--- src/api/service/api.js
+++ src/api/service/api.js
@@ -4,3 +4,13 @@ import api from '@/common/luch-request.js';
  * 获取发现页的分类
  */
 export const getDiscoveryCategories = () => api.get(`/discovery/categories`, {})
+
+/**s
+ * 获取推荐轮播图
+ */
+export const getRecommendRandom = (page) => api.get(`/recommend/random/${page}`, {})
+
+/**s
+ * 根据发现页分类id获取分类内容
+ */
+export const getDiscoveryMaterial = (materialId, page) => api.get(`/discovery/${materialId}/${page}`, {})
