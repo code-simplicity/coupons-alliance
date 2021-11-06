bd15b8711b0cf554f55be22e9982d7e61d4f6fbf
Romantic <72870719+dpy0912@users.noreply.github.com>
2021-11-06 19:35:23 +0800

    搜索页

---
+++ src/pages/search/index.vue
@@ -0,0 +1,23 @@
+<template>
+	<view class="search"></view>
+</template>
+
+<script>
+/**
+ * author	bugdr
+ * time     2021-11-5 8:57:37 ?F10: PM?
+ * description   搜索
+ */
+
+export default {
+	name: 'Search',
+	data() {
+		return {};
+	},
+	component: {},
+	mounted() {},
+	methods: {}
+};
+</script>
+
+<style lang="scss" scoped></style>
