9aade9349257a4db58a5bf6a27872cc27ffba442
Romantic <72870719+dpy0912@users.noreply.github.com>
2021-11-06 19:35:46 +0800

    初步完成首页的编写

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -10,15 +10,91 @@
 		<!-- tabs -->
 		<view class="tabs flex-align">
 			<view class="tabs-list">
-				<u-tabs
-					:list="list"
-					:is-scroll="true"
-					:current="current"
-					@change="change"
-					active-color="#ff0000"
-				></u-tabs>
+				<view class="left">
+					<u-tabs-swiper
+						ref="uTabs"
+						:list="list"
+						:is-scroll="true"
+						:current="current"
+						@change="change"
+						name="title"
+						active-color="#ff0000"
+					></u-tabs-swiper>
+				</view>
+				<view class="right"><u-icon name="list" color="#19114c" size="46"></u-icon></view>
 			</view>
-			<view class="right"><u-icon name="list" color="#19114c" size="46"></u-icon></view>
+		</view>
+
+		<view class="list-wrap">
+			<swiper
+				class="swiper"
+				:style="{ height: screenHeight + 'px' }"
+				:current="swiperCurrent"
+				@transition="transition"
+				@animationfinish="animationfinish"
+			>
+				<swiper-item>
+					<scroll-view scroll-y="true" style="height: 100%;">
+						<unt-recommed-loop></unt-recommed-loop>
+						<unt-recommed-list ref="productList0"></unt-recommed-list>
+					</scroll-view>
+				</swiper-item>
+				<swiper-item>
+					<scroll-view scroll-y="true" style="height: 100%;">
+						<unt-recommed-list ref="productList1"></unt-recommed-list>
+					</scroll-view>
+				</swiper-item>
+				<swiper-item>
+					<scroll-view scroll-y="true" style="height: 100%;">
+						<unt-recommed-list ref="productList2"></unt-recommed-list>
+					</scroll-view>
+				</swiper-item>
+				<swiper-item>
+					<scroll-view scroll-y="true" style="height: 100%;">
+						<unt-recommed-list ref="productList3"></unt-recommed-list>
+					</scroll-view>
+				</swiper-item>
+				<swiper-item>
+					<scroll-view scroll-y="true" style="height: 100%;">
+						<unt-recommed-list ref="productList4"></unt-recommed-list>
+					</scroll-view>
+				</swiper-item>
+				<swiper-item>
+					<scroll-view scroll-y="true" style="height: 100%;">
+						<unt-recommed-list ref="productList5"></unt-recommed-list>
+					</scroll-view>
+				</swiper-item>
+				<swiper-item>
+					<scroll-view scroll-y="true" style="height: 100%;">
+						<unt-recommed-list ref="productList6"></unt-recommed-list>
+					</scroll-view>
+				</swiper-item>
+				<swiper-item>
+					<scroll-view scroll-y="true" style="height: 100%;">
+						<unt-recommed-list ref="productList7"></unt-recommed-list>
+					</scroll-view>
+				</swiper-item>
+				<swiper-item>
+					<scroll-view scroll-y="true" style="height: 100%;">
+						<unt-recommed-list ref="productList8"></unt-recommed-list>
+					</scroll-view>
+				</swiper-item>
+				<swiper-item>
+					<scroll-view scroll-y="true" style="height: 100%;">
+						<unt-recommed-list ref="productList9"></unt-recommed-list>
+					</scroll-view>
+				</swiper-item>
+				<swiper-item>
+					<scroll-view scroll-y="true" style="height: 100%;">
+						<unt-recommed-list ref="productList10"></unt-recommed-list>
+					</scroll-view>
+				</swiper-item>
+				<swiper-item>
+					<scroll-view scroll-y="true" style="height: 100%;">
+						<unt-recommed-list ref="productList11"></unt-recommed-list>
+					</scroll-view>
+				</swiper-item>
+			</swiper>
 		</view>
 	</view>
 </template>
@@ -37,33 +113,157 @@ export default {
 			// 搜索关键字
 			keyword: '',
 			// 分类
-			list: [],
-			current: 0
+			list: [
+				{
+					id: 9660,
+					title: '推荐'
+				},
+				{
+					id: 9649,
+					title: '食品'
+				},
+				{
+					id: 9654,
+					title: '男装'
+				},
+				{
+					id: 9658,
+					title: '女装'
+				},
+				{
+					id: 9652,
+					title: '内衣'
+				},
+				{
+					id: 9650,
+					title: '母婴'
+				},
+				{
+					id: 9656,
+					title: '数码家电'
+				},
+				{
+					id: 9653,
+					title: '美妆个护'
+				},
+				{
+					id: 9651,
+					title: '运动户外'
+				},
+				{
+					id: 9648,
+					title: '鞋包配饰'
+				},
+				{
+					id: 9655,
+					title: '家居家装'
+				}
+			],
+			current: 0,
+			// 屏幕高度
+			screenHeight: 0,
+			swiperCurrent: 0
 		};
+	},
+	onReady() {
+		uni.getSystemInfo({
+			success: res => {
+				this.screenHeight = res.screenHeight;
+			}
+		});
 	},
 	component: {},
 	mounted() {
-		this.getDiscoveryCategories();
+		this.changeTabs(this.list, this.current);
+		// this.getDiscoveryCategories();
 	},
 	methods: {
 		// 获取分类
-		async getDiscoveryCategories() {
-			let res = await this.$api.getDiscoveryCategories();
-			if (res.code === 10000) {
-				res.data.map(item => {
-					let list = {
-						name: '',
-						id: ''
-					};
-					list.name = item.title;
-					list.id = item.id;
-					this.list.push(list);
-				});
+		// async getDiscoveryCategories() {
+		// 	let res = await this.$api.getDiscoveryCategories();
+		// 	if (res.code === 10000) {
+		// 		this.list = res.data;
+		// 		this.changeTabs(this.list, this.current);
+		// 	}
+		// },
+
+		// 切换tabs，传递materialId
+		changeTabs(list, index) {
+			switch (index) {
+				case 0: {
+					this.$refs.productList0.getDiscoveryMaterial(list[index].id);
+					break;
+				}
+				case 1: {
+					this.$refs.productList1.getDiscoveryMaterial(list[index].id);
+					break;
+				}
+				case 2: {
+					this.$refs.productList2.getDiscoveryMaterial(list[index].id);
+					break;
+				}
+				case 3: {
+					this.$refs.productList3.getDiscoveryMaterial(list[index].id);
+					break;
+				}
+				case 4: {
+					this.$refs.productList4.getDiscoveryMaterial(list[index].id);
+					break;
+				}
+				case 5: {
+					this.$refs.productList5.getDiscoveryMaterial(list[index].id);
+					break;
+				}
+				case 6: {
+					this.$refs.productList6.getDiscoveryMaterial(list[index].id);
+					break;
+				}
+				case 7: {
+					this.$refs.productList7.getDiscoveryMaterial(list[index].id);
+					break;
+				}
+				case 8: {
+					this.$refs.productList8.getDiscoveryMaterial(list[index].id);
+					break;
+				}
+				case 9: {
+					this.$refs.productList9.getDiscoveryMaterial(list[index].id);
+					break;
+				}
+				case 10: {
+					this.$refs.productList10.getDiscoveryMaterial(list[index].id);
+					break;
+				}
+				case 11: {
+					this.$refs.productList11.getDiscoveryMaterial(list[index].id);
+					break;
+				}
 			}
 		},
 
+		// 点击切换tabs
 		change(index) {
 			this.current = index;
+			this.swiperCurrent = index;
+			this.changeTabs(this.list, index);
+		},
+
+		// swiper-item左右移动，通知tabs的滑块跟随移动
+		transition(e) {
+			let dx = e.detail.dx;
+			this.$refs.uTabs.setDx(dx);
+		},
+
+		// 由于swiper的内部机制问题，快速切换swiper不会触发dx的连续变化，需要在结束时重置状态
+		// swiper滑动结束，分别设置tabs和swiper的状态
+		animationfinish(e) {
+			let current = e.detail.current;
+			this.$refs.uTabs.setFinishCurrent(current);
+			this.swiperCurrent = current;
+			if (this.current !== current) {
+				this.changeTabs(this.list, current);
+			}
+			this.current = current;
 		}
 	}
 };
@@ -79,9 +279,29 @@ export default {
 		flex: 1;
 	}
 	.tabs {
+		position: fixed;
+		left: 0;
+		right: 0;
+		z-index: 9;
 		background-color: #ffffff;
 		.tabs-list {
-			width: 88%;
+			width: 100%;
+			position: relative;
+			background-color: #ffffff;
+			display: flex;
+			align-items: center;
+			.left {
+				width: 90%;
+				overflow: hidden;
+			}
+			.right {
+				right: 10rpx;
+			}
+		}
+	}
+	.list-wrap {
+		.swiper {
+			margin-top: 90rpx;
 		}
 	}
 }
