92561468ddc1930cb7c5af972c9735f4c90fdb91
Romantic <72870719+dpy0912@users.noreply.github.com>
2021-11-07 20:37:43 +0800

    接口地址的改变

--- src/api/config.js
+++ src/api/config.js
@@ -3,9 +3,11 @@ let api_base_url = ''
 if (process.env.NODE_ENV === 'development') {
 	// 开发地址，直接使用服务器部署的地址
 	api_base_url = 'https://tb.romance-to-death.top/shop'
+	// api_base_url = 'https://api.sunofbeach.net/shop'
 } else if (process.env.NODE_ENV === 'production') {
 	// 生产模式的地址
 	api_base_url = 'https://tb.romance-to-death.top/shop'
+	// api_base_url = 'https://api.sunofbeach.net/shop'
 }
 
 // 接口地址统一暴露
