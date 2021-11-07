d82e9aa6ca139cb50ffd3a33cef3ae87c66f9d4b
Romantic <72870719+dpy0912@users.noreply.github.com>
2021-11-07 20:37:58 +0800

    添加一些接口地址

--- src/api/service/api.js
+++ src/api/service/api.js
@@ -10,7 +10,29 @@ export const getDiscoveryCategories = () => api.get(`/discovery/categories`, {})
  */
 export const getRecommendRandom = (page) => api.get(`/recommend/random/${page}`, {})
 
-/**s
+/**
  * 根据发现页分类id获取分类内容
  */
 export const getDiscoveryMaterial = (materialId, page) => api.get(`/discovery/${materialId}/${page}`, {})
+
+/**
+ * 搜索内容
+ */
+export const getSearch = params => api.get(`/search`, {
+	params
+})
+
+/**
+ * 券场 -精选
+ */
+export const getRecommendCategories = params => api.get(`/recommend/categories`, {})
+
+/**
+ * 券场 -精选内容
+ */
+export const getRecommendCategory = categoryId => api.get(`/recommend/${categoryId}`, {})
+
+/**
+ * 特惠接口
+ */
+export const getOnSell = page => api.get(`/onSell/${page}`, {})
